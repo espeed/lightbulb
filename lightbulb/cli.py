9a0216a6f2fd751ef7d8a85863618673e7ede942
James Thornton <james.thornton@gmail.com>
2012-02-29 01:57:13 -0600

    added new entry code to cli

--- lightbulb/cli.py
+++ lightbulb/cli.py
@@ -1,17 +1,59 @@
 import os
 import sys
+import uuid
+import datetime
+from string import Template
 
-from .engine import Config, Builder
+from .builder import Builder
+from .config import Config
+
+
+def get_template(template_path):
+    fin = open(template_path, "r")
+    text = fin.read().decode('utf-8')  # source_text
+    return Template(text)
+
+def create_new_file():
+
+    # TODO: parse out docid, maybe sign docid
+
+    filename = "another-test.rst"    
+
+    docid = uuid.uuid4().hex
+    author = "james"
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    params = dict(docid=docid, author=author, date=date)
+
+    template_path = "template.rst"
+    template = get_template(template_path)
+    content = template.substitute(params)
+
+    source_path = "source/%s" % filename
+    fout = open(source_path, "w")
+    fout.writelines(content)
+
+    fout.close()
+
 
 def main():
-    if len(sys.argv) not in (1, 2):
-        print >> sys.stderr, 'usage: lightbulb <folder>'
+    if len(sys.argv) not in (1, 2, 3):
+        print >> sys.stderr, 'usage: lightbulb <action> <folder>'
 
     if len(sys.argv) >= 2:
+        action = sys.argv[1]
+    else:
+        action = 'build'
+
+    if len(sys.argv) >= 3:
         folder = sys.argv[2]
     else:
         folder = os.getcwd()
 
+    if action not in ('build', 'serve'):
+        print >> sys.stderr, 'unknown action', action
+
+    
     config = Config(folder)
     builder = Builder(config)
 
--- lightbulb/engine.py
+++ lightbulb/engine.py
@@ -12,7 +12,6 @@ class Config(object):
     def __init__(self, project_folder=None):
         self.project_folder = project_folder or os.getcwd()
         self.source_folder = "source"
-        self.template_folder = "_templates"
         self.build_folder = "build"
         self.timezone = "America/Chicago"
 
